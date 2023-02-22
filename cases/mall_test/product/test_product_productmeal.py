b26bd1100f561f9d33e7cf399d7e22a7204a2a7d
王勇俊 <wangyj7@wedoctor.com>
2023-02-22 18:00:53 +0800

    新增接口用例

---
+++ cases/mall_test/product/test_product_productmeal.py
@@ -0,0 +1,37 @@
+import json
+import logging
+import time
+from datetime import datetime
+
+import pytest
+import allure
+
+import module.globals as gbl
+from untils.common_method import check_value_true, check_value_false, check_list_value
+from untils.db_tool import selectDBData
+from untils.db_tool import updateDBData
+
+
+@allure.epic("产品模块 - 产品套餐")
+@allure.feature("产品模块 - 产品套餐")
+class TestProductOps:
+    @allure.story("根据产品套餐编码获取产品套餐详情")
+    @allure.title("根据产品套餐编码获取产品套餐详情")
+    @pytest.mark.test
+    @pytest.mark.p0
+    def test_get_product_meal(self):
+        with allure.step("调用【根据产品套餐编码获取产品套餐详情】接口"):
+            req_source = 'H5'
+            req_product_list = ["HYQGCSTC001"]
+            response = gbl.productObj.get_product_meal(source=req_source, product_list=req_product_list)
+            data = response['data'][0]
+            res_mealName = data['mealName']
+            res_sourceCode = data['sourceCode']
+            res_tagCode = data['tagCode']
+            res_skuids = data['skuids']
+            res_productVos = data['productVos'][0]['skuid']
+            check_value_true('杭研全国测试套餐', res_mealName)
+            check_value_true('HYQGCSTC001', res_sourceCode)
+            check_value_true('HY_QG_CS001', res_tagCode)
+            check_value_false('', res_skuids)
+            check_value_false('', res_productVos)
---
+++ cases/mall_test/product/test_product_video.py
@@ -0,0 +1,64 @@
+import json
+import logging
+import time
+from datetime import datetime
+
+import pytest
+import allure
+
+import module.globals as gbl
+from untils.common_method import check_value_true, check_value_false, check_list_value
+from untils.db_tool import selectDBData
+from untils.db_tool import updateDBData
+
+
+@allure.epic("产品模块 - 视频")
+@allure.feature("产品模块 - 视频")
+class TestProductOps:
+    @allure.story("根据类目获取视频列表")
+    @allure.title("根据类目获取视频列表")
+    @pytest.mark.test
+    @pytest.mark.p0
+    def test_get_product_meal(self):
+        with allure.step("调用【根据类目获取视频列表】接口"):
+            req_baseAddressRef = 'CN'
+            req_categoryRef = '1'
+            req_merchantCode = 'yd'
+            response = gbl.productObj.get_video_list(baseAddressRef=req_baseAddressRef, categoryRef=req_categoryRef, merchantCode=req_merchantCode)
+            data = response['data']['resultData'][0]
+            res_id = data['id']
+            res_title = data['title']
+            res_categoryRef = data['categoryRef']
+            res_titleCapital = data['titleCapital']
+            res_imgUrl = data['imgUrl']
+            res_playAmount = data['playAmount']
+            check_value_true('CN009', res_id)
+            check_value_true('去妇科看病，碰到男医生怎么办？', res_title)
+            check_value_true('1', res_categoryRef)
+            check_value_true('QFKKB，PDNYSZMB？', res_titleCapital)
+            check_value_false('', res_imgUrl)
+            check_value_false(0, res_playAmount)
+
+    @allure.story("根据视频ID获取视频详情")
+    @allure.title("根据视频ID获取视频详情")
+    @pytest.mark.test
+    @pytest.mark.p0
+    def test_get_product_meal(self):
+        with allure.step("调用【根据视频ID获取视频详情】接口"):
+            req_baseAddressRef = 'CN'
+            req_videoRef = 'CN009'
+            req_merchantCode = 'yd'
+            response = gbl.productObj.get_video_detail(baseAddressRef=req_baseAddressRef, videoRef=req_videoRef, merchantCode=req_merchantCode)
+            data = response['data']
+            res_videoRef = data['videoRef']
+            res_title = data['title']
+            res_titleCapital = data['titleCapital']
+            res_introduce = data['introduce']
+            res_videoUrl = data['videoUrl']
+            res_labels = data['labels'][0]
+            check_value_true(req_videoRef, res_videoRef)
+            check_value_true('去妇科看病，碰到男医生怎么办？', res_title)
+            check_value_true('QFKKB，PDNYSZMB？', res_titleCapital)
+            check_value_false('', res_introduce)
+            check_value_false('', res_videoUrl)
+            check_value_false('', res_labels)
